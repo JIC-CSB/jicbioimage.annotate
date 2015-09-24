313cfa94d1540074ddc0f0a6faec5322ba0da979
Tjelvar Olsson <tjelvar.olsson@jic.ac.uk>
2015-09-24 23:02:49 +0100

    Added the beginnings of an AnnotationCanvas class.

--- jicbioimage/annotate/__init__.py
+++ jicbioimage/annotate/__init__.py
@@ -1,3 +1,43 @@
 """jicbioimage.annotate package."""
 
+import numpy as np
+
 __version__ = "0.0.1"
+
+
+class AnnotationCanvas(np.ndarray):
+    """Class for building up annotated images."""
+
+    @staticmethod
+    def blank_canvas(x, y):
+        """Return a blank canvas to annotate.
+
+        :param x: xdim
+        :param y: ydim
+        :returns: :class:`jicbioimage.annotate.AnnotationImage`
+        """
+        canvas = np.zeros((x, y, 3), dtype=np.uint8)
+        return canvas.view(AnnotationCanvas)
+
+    def draw_cross(self, x, y, color=(255, 0, 0), radius=4):
+        """Draw a cross on the canvas.
+
+        :param x: x coordinate (int)
+        :param y: y coordinate (int)
+        :param color: color of the cross (RGB tuple)
+        :param radius: radius of the cross (int)
+        """
+        for xmod in np.arange(-radius, radius+1, 1):
+            xpos = x + xmod
+            if xpos < 0:
+                continue  # Negative indices will draw on the opposite side.
+            if xpos >= self.shape[0]:
+                continue  # Out of bounds.
+            self[xpos, y] = color
+        for ymod in np.arange(-radius, radius+1, 1):
+            ypos = y + ymod
+            if ypos < 0:
+                continue  # Negative indices will draw on the opposite side.
+            if ypos >= self.shape[1]:
+                continue  # Out of bounds.
+            self[x, ypos] = color
--- tests/tests.py
+++ tests/tests.py
@@ -3,6 +3,8 @@ import os
 import os.path
 import shutil
 
+import numpy as np
+
 HERE = os.path.dirname(__file__)
 DATA_DIR = os.path.join(HERE, 'data')
 TMP_DIR = os.path.join(HERE, 'tmp')
@@ -10,13 +12,54 @@ TMP_DIR = os.path.join(HERE, 'tmp')
 
 class UnitTests(unittest.TestCase):
 
-    def test_can_import_package(self):
-        # Raises import error if the package cannot be imported.
-        import jicbioimage.annotate
-
     def test_package_has_version_string(self):
         import jicbioimage.annotate
         self.assertTrue(isinstance(jicbioimage.annotate.__version__, str))
+
+
+class AnnotationCanvasUnitTests(unittest.TestCase):
+
+    def test_blank_canvas(self):
+        from jicbioimage.annotate import AnnotationCanvas
+        canvas = AnnotationCanvas.blank_canvas(x=4, y=5)
+        self.assertTrue(isinstance(canvas, AnnotationCanvas))
+        self.assertTrue(isinstance(canvas, np.ndarray))
+        self.assertEqual(canvas.shape, (4, 5, 3))
+        self.assertEqual(canvas.dtype, np.uint8)
+        self.assertEqual(np.sum(canvas), 0)
+
+    def test_draw_cross(self):
+        from jicbioimage.annotate import AnnotationCanvas
+        canvas = AnnotationCanvas.blank_canvas(x=3, y=3)
+        canvas.draw_cross(x=1, y=1, color=(1, 1, 1), radius=1)
+        layer = np.array([[0, 1, 0], [1, 1, 1], [0, 1, 0]], dtype=np.uint8)
+        expected = np.dstack([layer, layer, layer])
+        self.assertTrue(np.array_equal(canvas, expected))
+
+    def test_draw_cross_in_upper_left_corner(self):
+        from jicbioimage.annotate import AnnotationCanvas
+        canvas = AnnotationCanvas.blank_canvas(x=3, y=3)
+        canvas.draw_cross(x=0, y=0, color=(1, 1, 1), radius=1)
+        layer = np.array([[1, 1, 0], [1, 0, 0], [0, 0, 0]], dtype=np.uint8)
+        expected = np.dstack([layer, layer, layer])
+        self.assertTrue(np.array_equal(canvas, expected))
+
+    def test_draw_cross_in_lower_right_corner(self):
+        from jicbioimage.annotate import AnnotationCanvas
+        canvas = AnnotationCanvas.blank_canvas(x=3, y=3)
+        canvas.draw_cross(x=2, y=2, color=(1, 1, 1), radius=1)
+        layer = np.array([[0, 0, 0], [0, 0, 1], [0, 1, 1]], dtype=np.uint8)
+        expected = np.dstack([layer, layer, layer])
+        self.assertTrue(np.array_equal(canvas, expected))
+
+    def test_cross_outside_canvas_raises_index_error(self):
+        from jicbioimage.annotate import AnnotationCanvas
+        canvas = AnnotationCanvas.blank_canvas(x=3, y=3)
+        with self.assertRaises(IndexError):
+            canvas.draw_cross(x=3, y=1, color=(1, 1, 1), radius=1)
+        with self.assertRaises(IndexError):
+            canvas.draw_cross(x=1, y=3, color=(1, 1, 1), radius=1)
+
 
 class FunctionalTests(unittest.TestCase):
 
@@ -26,6 +69,22 @@ class FunctionalTests(unittest.TestCase):
 
     def tearDown(self):
         shutil.rmtree(TMP_DIR)
+
+    def test_create_annotation_image_from_scratch(self):
+        from jicbioimage.annotate import AnnotationCanvas
+
+        # Create an empty canvas.
+        canvas = AnnotationCanvas.blank_canvas(50, 75)
+
+        # Draw a cross on it, centered on pixel 10,15.
+        canvas.draw_cross(x=10, y=15, radius=1, color=(1, 0, 0))
+
+        self.assertEqual(np.sum(canvas), 5)
+        self.assertEqual(canvas[10, 15, 0], 1)
+        self.assertEqual(canvas[9, 15, 0], 1)
+        self.assertEqual(canvas[11, 15, 0], 1)
+        self.assertEqual(canvas[10, 14, 0], 1)
+        self.assertEqual(canvas[10, 16, 0], 1)
 
 
 if __name__ == "__main__":
