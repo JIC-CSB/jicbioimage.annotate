d88ddf81ee3eb3949677e2ef746af8169ce88092
Tjelvar Olsson <tjelvar.olsson@jic.ac.uk>
2016-11-01 11:52:22 +0000

    Updated version number to 0.6.1

--- jicbioimage/illustrate/__init__.py
+++ jicbioimage/illustrate/__init__.py
@@ -42,7 +42,7 @@ import skimage.draw
 
 import jicbioimage.core.image
 
-__version__ = "0.6.0"
+__version__ = "0.6.1"
 
 HERE = os.path.dirname(__file__)
 DEFAULT_FONT_PATH = os.path.join(HERE, "fonts", "UbuntuMono-R.ttf")
